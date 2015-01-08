d59ed2c4c6e7a153bd7faccd1464c6859ef20b68
Tyler Willingham <tylerwillingham@gmail.com>
2015-01-07 20:49:30 -0600

    Added the MaintenanceItem model

--- maintenance_history/models.py
+++ maintenance_history/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from cars.models import Car
+
+
+class MaintenanceItem(models.Model):
+    vehicle = models.ForeignKey(Car)
+    type = models.ForeignKey(MaintenanceType)
+    summary = models.TextField()
+    performed_date = models.DateField()
+    date_entered = models.DateTimeField(auto_now_add=True)
+    date_last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.summary
 
 
 class MaintenanceType(models.Model):

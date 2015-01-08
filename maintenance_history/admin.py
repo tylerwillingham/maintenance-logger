8762e3e7077ae8dc070dc1f329d3b3cf4c947801
Tyler Willingham <tylerwillingham@gmail.com>
2015-01-08 11:36:33 -0600

    Registered the maintenance_history models with django admin

--- maintenance_history/admin.py
+++ maintenance_history/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.MaintenanceItem)
+admin.site.register(models.MaintenanceType)
+admin.site.register(models.MaintenanceReceipt)

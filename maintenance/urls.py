683a24b7ad844134d374aac5ffc1a54d82c82f80
Tyler Willingham <tylerwillingham@gmail.com>
2015-01-09 23:09:11 -0600

    Added the rest_framework browsable api in urls.py

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # rest framework browsable api
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )

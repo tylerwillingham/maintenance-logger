e6640a9de10d3a56747182829e3706d5019ced0f
Tyler Willingham <tylerwillingham@gmail.com>
2015-01-09 22:54:29 -0600

    Installed Django Rest Framework + Markdown + Django Filter

--- maintenance/settings.py
+++ maintenance/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party
+    'rest_framework',
+
+    # Mine
     'cars',
     'maintenance_history',
 )

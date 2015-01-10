0e9c5ef1ec143a5e789a2e600f5d7838efe17e3f
Tyler Willingham <tylerwillingham@gmail.com>
2015-01-09 23:12:00 -0600

    Set temp permissions for the rest framework

--- maintenance/settings.py
+++ maintenance/settings.py
@@ -88,3 +88,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Rest framework
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
